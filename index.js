5eaf3a8fcc7c93115c340793b4fc289b08e3d9f4
lauuurars <1120098177@u.icesi.edu.co>
2025-08-21 17:47:07 -0500

    mejorando info

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const persona = {
 
 class Persona {
     constructor(nombre, edad, profesion) {
-        this.nombre = nombre;
+        this.nombre = nombre; //this sirve para guardar dentro del contexto constructor las propiedades.
         this.edad = edad;
         this.profesion = profesion;
-        this._passwordpassword = "12345"; //propiedad oculta
+        this._passwordpassword = "12345"; //propiedad oculta usando ._
     }
 
     hablar() {
-    console.log(`Hola, soy ${this.nombre} y yo soy ${this.profesion}.`);
+        console.log(`Hola, soy ${this.nombre} y yo soy ${this.profesion}.`);
     }
 
     get password() {
@@ -24,7 +24,7 @@ class Persona {
     }
 
     set edad(edad) {
-        if (edad > 0){
+        if (edad > 0) {
             this._edad = edad;
         } else {
             console.log('Edad no valida');
@@ -32,12 +32,40 @@ class Persona {
     }
 }
 
-//constructor nos permite crear las
+//constructor nos permite crear objetos y asignarles valores a las propiedades de ese objeto.
+//Un constructor sirve para que cuando creemos un nuevo objeto, ya venga preparado con sus datos iniciales.
 
 const laura = new Persona("Laura", 18, "Diseñadora :3"); //ingreso las características que corresponden al objeto.
-const camila = new Persona ("Camila", 28, "Arquitecta");
+const camila = new Persona("Camila", 28, "Arquitecta");
+laura.edad = -20;
+
+console.log(laura);
 console.log(laura.nombre);
 console.log(laura.hablar());
+
+class Empleado extends Persona {
+    constructor(nombre, edad, profesion, salario) {
+        super(nombre, edad, profesion); //super hace referencia a la super clase (clase que hereda), ejecuta el constructor de Persona.
+        this.salario = salario;
+    }
+}
+
+class Estudiante extends Persona {
+    constructor(nombre, edad, profesion, curso) {
+        super(nombre, edad, profesion);
+        this.curso = curso;
+    }
+
+    estudiar() {
+        console.log(`${this.nombre} está estudiando ${this.curso}.`);
+    }
+}
+
+const sofia = new Estudiante("Sofía", 19, "Bachiller", "DCA");
+
+console.log(sofia.estudiar()); //Sofía está estudiando DCA.
+
+//----------------------- Algunos Apuntes ------------------------
 
 
 //instanciar = se refiere a que a partir de un molde creo un elemento específico de ese tipo.
@@ -47,9 +75,7 @@ console.log(laura.hablar());
 
 //trabajar en moldes significa que todos los objetos (personas) tendrán las mismas propiedades.
 
-//this sirve para guardar dentro del contexto constructor las propiedades.
-
-/*Getters y Setters: 
+/*Getters y Setters:
 
 Gettters: Cómo puedo obtener una información.
 Setters: Cómo cambiar/modificar una información.
@@ -58,9 +84,6 @@ atributos que consideramos privados this._valor.
 
 */
 
-//Herencia: se pueden extender y declarar nuevas características en una nueva varianble.
-//Lo ideal es agregar atributos de una sola clase.
-//super hace referencia a la super clase (clase que hereda), ejecuta el constructor de Persona.
+//Herencia: se pueden extender y declarar nuevas características en una nueva varianble. Lo ideal es agregar atributos de una sola clase.
 
 //Polimorfismo = múltiples formas de hacer algo.
-
